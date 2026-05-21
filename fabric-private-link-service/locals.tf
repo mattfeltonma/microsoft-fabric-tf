96ae1c7a669c7a507bfad4ce12dfc6df154e593b
mattfeltonma <matt.felton@feltonma.com>
2026-05-21 13:25:46 -0400

    modified - small update

--- fabric-private-link-service/locals.tf
+++ fabric-private-link-service/locals.tf
@@ -3,8 +3,8 @@ locals {
     ## Get the name of the private connection that was created in the template
     ## to be used for auto-approval
     private_endpoint_connection_name = element([
-        for connection in data.azapi_resource.fabric_private_link_service.properties.privateEndpointConnections 
+        for connection in jsondecode(azapi_resource.fabric_private_link_service.output).properties.privateEndpointConnections 
         : connection.name
-        if endswitch (connection.properties.privateEndpoint.id, fabric_workspace_managed_private_endpoint.fabric_workspace_mpe.name)
+        if endswith(connection.properties.privateEndpoint.id, fabric_workspace_managed_private_endpoint.fabric_workspace_mpe.name)
     ], 0)
 }
