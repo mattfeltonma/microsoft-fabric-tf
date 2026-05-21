25107d8e6162676ab3472177335f77d71d359c6e
mattfeltonma <matt.felton@feltonma.com>
2026-05-21 13:41:10 -0400

    modified

--- fabric-private-link-service/locals.tf
+++ fabric-private-link-service/locals.tf
@@ -2,7 +2,7 @@ locals {
 
     ## Get the name of the private connection that was created in the template
     ## to be used for auto-approval
-    fabric_pls_output = jsondecode(azapi_resource.fabric_private_link_service.output)
+    fabric_pls_output = azapi_resource.fabric_private_link_service.output
     
     private_endpoint_connection_name = try(
         local.fabric_pls_output.properties.privateEndpointConnections[0].name,
